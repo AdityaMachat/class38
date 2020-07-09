fd79e49e5821b423c291036a0a974f4bc4f73ce5
AdityaMachat <64791200+AdityaMachat@users.noreply.github.com>
2020-07-09 07:53:08 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,46 @@
-var database,canvas;
-var form,player,game;
-var gameState=0,playerCount;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
 var allPlayers;
-var car1,car2,car3,car4,cars;
-var carimg1,carimg2,carimg3,carimg4,track,ground;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+var hurdle ;
 
 function preload(){
-  carimg1=loadImage("images/car1.png");
-  carimg2=loadImage("images/car2.png");
-  carimg3=loadImage("images/car3.png");
-  carimg4=loadImage("images/car4.png");
-  track=loadImage("images/track.jpg");
-  ground=loadImage("images/ground.png");
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/player.png");
+  car2_img = loadImage("../images/player.png");
+  car3_img = loadImage("../images/player.png");
+  car4_img = loadImage("../images/player.png");
+  ground = loadImage("../images/ground.png");
 }
 
 function setup(){
-  database=firebase.database();
-  createCanvas(displayWidth-50,displayHeight-50);
-  game=new Game();
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
   game.getState();
   game.start();
 }
 
+
 function draw(){
-    if(playerCount===4){
-      game.update(1);
-    }
-    if(gameState===1){
-      clear();
-      game.play();
-    }
-    if(gameState===2){
-      game.end();
-    }
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
 }
